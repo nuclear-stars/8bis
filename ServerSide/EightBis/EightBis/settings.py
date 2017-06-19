833442794d9c8eb2e9a3661ff42b222ad5d0a21b
Eyal <eyalrs21@gmail.com>
2017-06-19 14:27:49 +0300

    basic DB models

--- ServerSide/EightBis/EightBis/settings.py
+++ ServerSide/EightBis/EightBis/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webservice.apps.WebserviceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ServerSide/EightBis/webservice/models.py
+++ ServerSide/EightBis/webservice/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+
+class Dish(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
