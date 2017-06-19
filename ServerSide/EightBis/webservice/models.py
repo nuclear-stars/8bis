fcc6770cddb57408ddb9f62e811928904ec8c49c
Eyal <eyalrs21@gmail.com>
2017-06-20 01:19:34 +0300

    Fix support unicode

--- ServerSide/EightBis/webservice/models.py
+++ ServerSide/EightBis/webservice/models.py
@@ -14,6 +14,7 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class DishCategory(models.Model):
     name = models.CharField(max_length=100)
 
