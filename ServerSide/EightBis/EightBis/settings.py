d56527e391f92c590e245f749e7a81681e79f111
Eyal <eyalrs21@gmail.com>
2017-06-19 14:14:50 +0300

    Configured mysql server

--- ServerSide/EightBis/EightBis/settings.py
+++ ServerSide/EightBis/EightBis/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'EightBis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '8bis',
+        'USER': 'user55',
+        'PASSWORD': 'My!hack@2017-55',
+        'HOST': '13.93.107.254',
+        'PORT': '3306'
     }
 }
 
