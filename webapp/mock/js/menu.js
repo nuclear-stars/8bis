76352f3b8afa2ac416924ee1d97a1d0dc1ca1937
Omer Perry <thebne@gmail.com>
2017-06-20 03:31:35 +0300

    added local storage support for reactions

--- webapp/mock/js/menu.js
+++ webapp/mock/js/menu.js
@@ -1,9 +1,58 @@
+function formatDate(date) {
+	if (date == null) date = Date.now();
+	var d = new Date(date),
+		month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+var storagePrefix = "menuStorage-";
+function getStorageValue(key) {
+	if (localStorage == undefined) return;
+	return localStorage.getItem(storagePrefix + formatDate() + "-" + key);
+}
+function setStorageValue(key, val) {
+	if (localStorage == undefined) return;
+	return localStorage.setItem(storagePrefix + formatDate() + "-" + key, val);
+}
+
+function changeReactionsStatus(dishId, icons) {
+	var reactionArr = new Array();
+	icons.find('.list a').each(function() {
+		var type = this.className.replace('selected', '').replace(' ', '');
+		setStorageValue(dishId + '-' + type, $(this).hasClass('selected'));
+		reactionArr[dishId] = $(this).hasClass('selected');
+	});
+	// todo submit reactionArr with ajax
+}
+
+function updateReactionsCount(icons) {
+	var selectedNum = icons.find('a.selected').length;
+	var count = icons.find('.count > span');
+	if (selectedNum == 0) {
+		count.hide();
+		return;
+	}
+	count.show().text(selectedNum);
+}
+
 $(function() {
 	$('.share-icons .list a').each(function() {
+		var type = this.className.replace('selected', '').replace(' ', '');
+		var dishId = $(this).closest('dt').data('dish-id');
+		if (getStorageValue(dishId + "-" + type) == "true") {
+			$(this).addClass('selected');
+		}
+		updateReactionsCount($(this).closest('.share-icons'));
+		
 		$(this).click(function() {
 			var isOn = !$(this).hasClass('selected');
 			$(this).toggleClass('selected');
-			var type = this.className.replace('selected', '').replace(' ', '');
 			var reaction = $(this).closest('.share-icons').siblings('.current-reactions').find('.' + type + ' > em');
 			newVal = isOn ? parseInt(reaction.text()) + 1 : parseInt(reaction.text()) - 1;
 			reaction.text(newVal);
@@ -12,14 +61,10 @@ $(function() {
 			} else {
 				reaction.parent().fadeOut(200);
 			}
-			
-			var selectedNum = $(this).parent().children('a.selected').length;
-			var count = $(this).closest('.share-icons').find('.count > span');
-			if (selectedNum == 0) {
-				count.hide();
-				return;
-			}
-			count.show().text(selectedNum);
+			var icons = $(this).closest('.share-icons');
+			changeReactionsStatus(dishId, icons);
+			updateReactionsCount(icons);
 		});
 	});
 });
+
